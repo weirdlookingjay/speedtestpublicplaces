4be0e79e91e6b5149b4694fbcbff08ca8612e4a6
Joe Clark <weirdlookingjay.com>
2023-04-07 16:07:08 -0400

    Added basic places list using reactjs

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
 
-class PlacesList extends React.Component {
-    render() {
-        return (
-            <div>
-                Places list rendered in react!              
-            </div>
-        )
+const PlacesList = () => {
+    const [loading, setLoading] = useState(true);
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+    useEffect(() => {
+        // Hit the server and get the places list
+        const apiEndpoint = "/api/places";
+        fetch(apiEndpoint)
+            .then(response => response.json())
+            .then(data => {
+                setLoadedPlaces(data["places"]) 
+                setLoading(false)
+            })
+
+    }, [])
+
+    const loadingSection = (<div>Loading...</div>);
+    const dataSection = loadedPlaces.map((place, index) => (
+        <div key={index}>
+            <table>
+                <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Recent Upload Speed</th>
+                    <th>Recent Upload Speed Units</th>
+                    <th>Number of measurements</th>
+                </tr>
+                <tr>
+                    <td>{place.name}</td>
+                    <td>{place.city}</td>
+                    <td>{place.most_recent_download_speed}</td>
+                    <td>{place.most_recent_download_unit}</td>
+                    <td>{place.number_of_measurements}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )) 
+   
+    if (loading) {
+        return loadingSection
+
+    } else {
+        return dataSection
     }
 }
+
 
 const placesList = ReactDOM.createRoot(document.getElementById("places-list-container"));
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "places#index"
+
+  get "/api/places", to: "api/places#index"
 end
